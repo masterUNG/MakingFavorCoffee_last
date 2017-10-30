6099736b40951ad8456b4d72d6b5450c91b23966
nut <nutchatchon@gmail.com>
2017-10-30 13:05:29 +0700

    Check Space OK

--- app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/fragment/MainFragment.java
+++ app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/fragment/MainFragment.java
@@ -6,15 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import rmutsb.mook.chatchon.makingfavorcoffee.R;
+import rmutsb.mook.chatchon.makingfavorcoffee.ultility.MyAlertDialog;
 
 /**
  * Created by WIN10-CREATOR on 13/9/2560.
  */
 
 public class MainFragment extends Fragment{
+
+    private String emailString, passwordString;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,7 +36,36 @@ public class MainFragment extends Fragment{
         //Register Controller
         registerController();
 
+//        SingIn Controller
+        singInController();
+
     } //onActivityCreated
+
+    private void singInController() {
+        Button button = getView().findViewById(R.id.btnSingIn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                emailString = emailEditText.toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                if (emailString.equals("")|| passwordString.equals("")) {
+                    MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+                    myAlertDialog.myDialog(getResources().getString(R.string.message_haveSpace),
+                            getResources().getString(R.string.message_haveSpace));
+                } else {
+
+                }
+
+
+
+            } //onClick
+        });
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtNewRegister);
