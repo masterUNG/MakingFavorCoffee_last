3e2ae0b305f3c06b503a1f371273490d74923077
nut <nutchatchon@gmail.com>
2017-10-30 15:11:25 +0700

    Hamberger Icon Ok

--- app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/CoffeeActivity.java
+++ app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/CoffeeActivity.java
@@ -1,14 +1,72 @@
 package rmutsb.mook.chatchon.makingfavorcoffee;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class CoffeeActivity extends AppCompatActivity {
+
+    //    Explicit
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private String[] loginStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee);
+
+        //Create Toolbar
+        createToolbar();
+
+
     } // Main Method
 
+    private void createToolbar() {
+        loginStrings = getIntent().getStringArrayExtra("Login");
+
+        toolbar = (Toolbar) findViewById(R.id.toolbarCoffee);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(loginStrings[1] + " " + loginStrings[2]);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerMenu);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(CoffeeActivity.this,
+                drawerLayout, R.string.open, R.string.close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+
+    } // CreateToolbar
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 } // Main Class
--- app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/MainActivity.java
+++ app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/MainActivity.java
@@ -1,11 +1,17 @@
 package rmutsb.mook.chatchon.makingfavorcoffee;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import rmutsb.mook.chatchon.makingfavorcoffee.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
